29562bdb0270bb6e3780ce5bce2c8d87a07840fc
Pinfinity <125202205+pinfinity1@users.noreply.github.com>
2026-09-14 18:08:14 +0330

    deploy

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -4,6 +4,7 @@ import { createNavigation } from "next-intl/navigation";
 export const routing = defineRouting({
   locales: ["fa", "en", "ar"],
   defaultLocale: "fa",
+  localeDetection: false,
 });
 
 export const { Link, redirect, usePathname, useRouter, getPathname } =
